599ca1c2526977310379f97878d9501920e2f220
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-10-24 21:17:45 +0700

    🔧 chore(package.json): update "@prisma/client" dependency version to "^5.1.1" to match the required version 🔧 chore(package.json): update "prisma" dependency version to "^5.1.1" to match the required version 🔧 chore(db.ts): remove commented out code and unused types, improve code formatting and add missing newline at end of file 🔧 chore(line.ts): remove unnecessary whitespace

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,34 +1,16 @@
 
-// import { PrismaClient } from '@prisma/client/edge';
-// import { env } from "~/env.mjs";
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClient | undefined;
-// };
-
-// export const db =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({
-//     log:
-//       env.APP_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-//   });
-
-// if (env.APP_ENV !== "production") globalForPrisma.prisma = db;
-
-
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 import { env } from "~/env.mjs";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
-}
+  prisma: PrismaClient | undefined;
+};
 
-export const db = globalForPrisma.prisma ?? prismaClientSingleton()
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      env.APP_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
 
-if (env.APP_ENV !== 'production') globalForPrisma.prisma = db
+if (env.APP_ENV !== "production") globalForPrisma.prisma = db;
