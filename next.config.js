c4b42edc5ceb10a56430203dcdcef4fd1a4f4d3a
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-10-24 09:38:22 +0700

    📦 chore(next.config.js): add next.config.js file to configure Next.js project 📦 chore(package.json): add @prisma/nextjs-monorepo-workaround-plugin as a dependency to enable Prisma plugin in Next.js 📦 chore(tsconfig.json): include next.config.js file in the list of files to be compiled by TypeScript

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
+// await import("./src/env.mjs");
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
 /** @type {import("next").NextConfig} */
-const config = {
+module.exports = {
   reactStrictMode: true,
 
   /**
@@ -29,6 +30,11 @@ const config = {
       },
     ],
   },
-};
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
 
-export default config;
+    return config
+  },
+};
