efe2aa65a0e361c6428d694f56ace630fe8289c7
Aphidet-Hongseetong <ottooffple@icloud.com>
2024-02-24 13:34:07 +0700

    Add AQI data and import it in airvisual.ts

--- src/services/airvisual.ts
+++ src/services/airvisual.ts
@@ -1,3 +1,4 @@
+import { AqiData } from '~/data/aqi_data';
 import { env } from '~/env.mjs';
 
 const getNearestCity = async (latitude: number, longitude: number): Promise<any> => {
@@ -14,54 +15,6 @@ const getNearestCity = async (latitude: number, longitude: number): Promise<any>
 };
 
 const getNearestCityBubble = (aqi: number, ts: string) => {
-  const aqiData: AQIData[] = [
-    {
-      level: 'green',
-      backgroundColor: '#A8DF5F',
-      boxImageColor: '#87C13C',
-      imageUrl: 'https://i.ibb.co/2g3LhbW/ic-face-green.png',
-      textColor: '#607631',
-      description: 'ดี',
-      pm25: '0-15 µg/m³',
-    },
-    {
-      level: 'yellow',
-      backgroundColor: '#FDD64B',
-      boxImageColor: '#EFBE1D',
-      imageUrl: 'https://i.ibb.co/L6v112C/ic-face-yellow.png',
-      textColor: '#8B6B1D',
-      description: 'ปานกลาง',
-      pm25: '16-35.4 µg/m³',
-    },
-    {
-      level: 'orange',
-      backgroundColor: '#FB9B57',
-      boxImageColor: '#F27E2F',
-      imageUrl: 'https://i.ibb.co/Z20zDcs/ic-face-orange.png',
-      textColor: '#974920',
-      description: 'มีผลกระทบต่อผู้ป่วยหรือร่างกายอ่อนแอ',
-      pm25: '35.5-65 µg/m³',
-    },
-    {
-      level: 'red',
-      backgroundColor: '#FA6A69',
-      boxImageColor: '#E84B51',
-      imageUrl: 'https://i.ibb.co/B26L61b/ic-face-red.png',
-      textColor: '#942431',
-      description: 'มีผลกระทบต่อทุกคน',
-      pm25: '66-150 µg/m³',
-    },
-    {
-      level: 'purple',
-      backgroundColor: '#A97ABC',
-      boxImageColor: '#8A5D9D',
-      imageUrl: 'https://i.ibb.co/3YRzknH/ic-face-purple.png',
-      textColor: '#543B63',
-      description: 'มีผลกระทบต่อทุกคนอย่างรุนแรง',
-      pm25: '201-250 µg/m³',
-    },
-  ];
-
   let level = '';
   switch (true) {
     case (aqi <= 50):
@@ -89,7 +42,7 @@ const getNearestCityBubble = (aqi: number, ts: string) => {
     hour12: false,
   });
 
-  const objAqi = aqiData.find((item: any) => item.level === level);
+  const objAqi = AqiData.find((item: any) => item.level === level);
 
   const bubble: any = [
     {
