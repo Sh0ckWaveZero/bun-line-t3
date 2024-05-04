60542568c03eaea3ba4e26dd410087a932ca69d5
Aphidet-Hongseetong <ottooffple@icloud.com>
2024-05-04 18:36:59 +0700

    chore: Remove commented out code for standalone output in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
-  output: "standalone",
+  // output: "standalone",
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
